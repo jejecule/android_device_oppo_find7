3f93cc72901ba6462517a2cb055b5073febff21e
Jejecule <papaelias2012@gmail.com>
2015-12-22 18:33:58 +0100

    remove no op

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7/mkbootimg.mk
 TARGET_KERNEL_CONFIG := cyanogenmod_find7_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
 # Bluetooth
